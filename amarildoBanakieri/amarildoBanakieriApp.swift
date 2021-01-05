acb25f645030b12d5b5efaba2fbbb017e0b76bb4
jjb <b>
2021-01-05 17:21:17 +0100

    Initial Commit

---
+++ amarildoBanakieri/amarildoBanakieriApp.swift
@@ -0,0 +1,17 @@
+//
+//  amarildoBanakieriApp.swift
+//  amarildoBanakieri
+//
+//  Created by User on 5.1.21.
+//
+
+import SwiftUI
+
+@main
+struct amarildoBanakieriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
